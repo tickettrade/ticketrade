825db2957ae6f7907543347baaab8f0ebcc1b3cc
matansaar <matansaar8@Gmail.com>
2026-05-29 00:01:48 +0300

    Remove conflicting Vite template files

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [] // Overrides parent postcss config to prevent tailwind module loading issues
-    }
-  }
-})
